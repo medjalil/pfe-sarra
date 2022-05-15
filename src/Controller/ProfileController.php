dda9f910b2f0a6b63487805c495392916a1d6fa0
Mohamed Abdeljalil <med.abdeljalil@gmail.com>
2022-05-15 23:28:24 +0100

    feat: update2

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,19 +2,102 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
+use App\Form\ProfileType;
+use App\Repository\ProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/profile")
+ */
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile", name="app_profile")
+     * @Route("/", name="app_profile_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(ProfileRepository $profileRepository): Response
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'profiles' => $profileRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/me", name="app_profile", methods={"GET"})
+     */
+    public function myProfile(ProfileRepository $profileRepository): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('profile/me.html.twig', [
+            'profile' => $profileRepository->find($user),
+        ]);
+
+    }
+
+    /**
+     * @Route("/new", name="app_profile_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ProfileRepository $profileRepository): Response
+    {
+        $user = $this->getUser();
+        $profile = new Profile();
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $profile->setOwner($user);
+            $profileRepository->add($profile);
+            return $this->redirectToRoute('app_profile', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile/new.html.twig', [
+            'profile' => $profile,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_profile_show", methods={"GET"})
+     */
+    public function show(Profile $profile): Response
+    {
+        return $this->render('profile/show.html.twig', [
+            'profile' => $profile,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_profile_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Profile $profile, ProfileRepository $profileRepository): Response
+    {
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $profileRepository->add($profile);
+            return $this->redirectToRoute('app_profile', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('profile/edit.html.twig', [
+            'profile' => $profile,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_profile_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Profile $profile, ProfileRepository $profileRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $profile->getId(), $request->request->get('_token'))) {
+            $profileRepository->remove($profile);
+        }
+
+        return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
     }
 }
