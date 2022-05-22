e2307ba23d3fbd62c0f3cfe0f61bd1befb9f0226
sarra555 <sarraaouadi01234@gmail.com>
2022-05-22 10:35:57 +0100

    update sarra

--- src/Form/DemandeType.php
+++ src/Form/DemandeType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Demande;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +16,45 @@ class DemandeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('budget')
+            ->add('title', TextType::class, [
+                'required' => true,
+                'label' => 'titre'
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'label' => 'description'
+            ])
+            ->add('budget', MoneyType::class, [
+                'required' => true,
+                'label' => 'budget'
+            ])
+           
+            ->add('address', TextareaType::class, [
+                'required' => true,
+                'label' => 'Adresse'
+            ])
+
+            ->add('postalCode', IntegerType::class, [
+                'required' => true,
+                'label' => 'Code postal'
+
+            ])
+
+            ->add('city')
+
+            ->add('status', TextType::class, [
+                'required' => true,
+                'label' => 'statue'
+            ])
+            
+            ->add('subCategory')
+
             ->add('startAt')
             ->add('endAt')
             ->add('limitAt')
-            ->add('address')
-            ->add('postalCode')
-            ->add('city')
-            ->add('createdAt')
-            ->add('status')
-            ->add('subCategory');
-    }
+            ->add('createdAt');
+
+    }       
 
     public function configureOptions(OptionsResolver $resolver): void
     {
